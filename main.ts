5cd47be57632a93a2accb0faa7ddc1e542b5d9de
8088567-eng <8088567@student.harmonytx.org>
2026-01-12 10:29:12 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,22 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.StickFigure)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.StickFigure)
+        basic.pause(5000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # #
+            # # # # #
+            . # . . #
+            `)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
 })
-basic.showIcon(IconNames.Duck)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . # # # #
+    # # # # #
+    . # . . #
+    `)
