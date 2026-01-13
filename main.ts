37dd620a9ce0e0f2ec14641d87cc62df602e6644
Shubham-cloud936 <8020795@student.harmonytx.org>
2026-01-13 10:19:35 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 input.onButtonPressed(Button.A, function () {
 	
 })
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . # # # #
+    # # # # #
+    . # . . #
+    `)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # # # #
-            # # # # #
-            . # . . #
-            `)
         basic.pause(2000)
         basic.showIcon(IconNames.StickFigure)
     } else {
