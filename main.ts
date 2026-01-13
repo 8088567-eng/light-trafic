6c8b196fe7bc378259ec863c87f44370770361f0
Shubham-cloud936 <8020795@student.harmonytx.org>
2026-01-13 10:17:56 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         basic.showIcon(IconNames.StickFigure)
         basic.pause(5000)
