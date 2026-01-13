572c476f491152428d99f13f7058951461c487e7
Shubham-cloud936 <8020795@student.harmonytx.org>
2026-01-13 10:32:33 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.StickFigure)
     basic.pause(5000)
-    music.play(music.stringPlayable("C5 - C5 - C5 - C5 - ", 100), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C5 - C5 - C5 - C5 - ", 60), music.PlaybackMode.UntilDone)
+    basic.showNumber(5)
+    basic.showNumber(4)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
     basic.showLeds(`
         . . . . .
         . . . . .
